657a78a96877d5f58ae39f557ce4d75b06b9e4ab
AdityaVikramSingh45 <adityavikramshubh234@gmail.com>
2025-05-31 22:36:15 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000"], // React dev server
+    origin: "http://localhost:3000", // React dev server
     credentials: true,
   })
 );
@@ -60,6 +60,7 @@ app.get("/api/v1", (req, res) => {
 // -------------------------
 // Serve React Frontend Build
 // -------------------------
+// Serve static frontend files
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", (req, res) => {
