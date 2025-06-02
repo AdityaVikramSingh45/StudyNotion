e6cba21fbd147f1a3fa44a285dd9a1b113277526
AdityaVikramSingh45 <adityavikramshubh234@gmail.com>
2025-06-02 18:05:51 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -64,11 +64,11 @@ app.get("/api/v1", (req, res) => {
 // Serve React Frontend Build
 // -------------------------
 // Serve static frontend files
-app.use(express.static(path.join(__dirname, "build")));
+// app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
 // -------------------------
 
